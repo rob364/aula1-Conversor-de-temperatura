5abc2c66f3228b2ed4dcafcfa0c2482bd91dad9f
Roberta <vcindustria@gmail.com>
2022-05-19 22:19:14 -0300

    Primeiro comite

---
+++ conteudo/ex.js
@@ -0,0 +1,15 @@
+
+var nome = "Roberta"
+var notaDoPrimeiroBimestre = 9.4355
+var notaDoSegundoBimestre = 7.234
+var notaDoTerceiroBimestre = 4.234
+var notaDoQuartoBimestre = 2.183
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+/* Revisão
+Variaveis, string, console.log, toFixed, operações, matematica, concatenação */
